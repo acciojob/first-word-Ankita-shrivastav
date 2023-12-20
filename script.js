55dc2df867143896043b3fcd0cbde6c4e9992e1a
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-21 00:47:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
   // your code here
+	if(str===''){
+		return '';
+	}
+	cont words=str.split('');
+	return words[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
